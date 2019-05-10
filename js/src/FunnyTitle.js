5bf90f60bf5a2d35e00276745bdbbac5ea75bee4
tsuki419 <1433092802@qq.com>
2019-05-10 23:44:36 +0800

    Site updated: 2019-05-10 23:44:29

--- js/src/FunnyTitle.js
+++ js/src/FunnyTitle.js
@@ -3,12 +3,12 @@
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         $('[rel="icon"]').attr('href', "/img/trhx1.png");
          document.title = 'ヽ(●-`Д´-)ノ别回来了哼！';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         $('[rel="icon"]').attr('href', "/img/trhx1.png");
          document.title = 'ヾ(Ő∀Ő3)ノ就知道你还爱我！' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
